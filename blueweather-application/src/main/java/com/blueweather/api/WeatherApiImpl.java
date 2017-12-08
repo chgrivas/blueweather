8ea6a73ea9658a986c3982cc482ce1f4609aeaca
Chris Grivas <chgrivas7@gmail.com>
2017-12-08 21:08:54 +0200

    Handle errors

--- blueweather-application/src/main/java/com/blueweather/api/WeatherApiImpl.java
+++ blueweather-application/src/main/java/com/blueweather/api/WeatherApiImpl.java
@@ -4,10 +4,13 @@ import com.blueweather.api.dto.WeatherResponseDto;
 import com.blueweather.domain.enumeration.Place;
 import com.blueweather.service.WeatherRetrievalService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.web.ErrorAttributes;
+import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
@@ -15,7 +18,7 @@ import java.io.IOException;
 import java.time.Instant;
 
 @RestController
-public class WeatherApiImpl implements WeatherApi
+public class WeatherApiImpl implements WeatherApi, ErrorController
 {
   private static final String MAX_HUMIDITY_LABEL = "Max percentage humidity";
   private static final String MAX_TEMPERATURE_LABEL = "Max Temp in C";
@@ -23,8 +26,16 @@ public class WeatherApiImpl implements WeatherApi
   private static final String PRECIPATION_LABEL = "Precipation in mm";
   private static final String FILE_KEY_VALUE_SEPARATOR = ": ";
 
+  private static final String PATH = "/error";
+
+  @Value("${debug}")
+  private Boolean debugMode;
+
   @Autowired
   private WeatherRetrievalService weatherRetrievalService;
+
+  @Autowired
+  private ErrorAttributes errorAttributes;
 
   @Override
   public void getDefaultWeather(HttpServletResponse response) {
@@ -49,5 +60,17 @@ public class WeatherApiImpl implements WeatherApi
     } catch (IOException e) {
       e.printStackTrace();
     }
+  }
+
+  @Override
+  public String getErrorPath() {
+    return PATH;
+  }
+
+  @RequestMapping(value = PATH)
+  ResponseEntity<String> error(HttpServletResponse response) {
+    HttpHeaders responseHeaders = new HttpHeaders();
+    HttpStatus httpStatus = HttpStatus.valueOf(response.getStatus());
+    return new ResponseEntity<>("Cannot Process Your Request Right Now! Please Contact the Administrator for more detail.", responseHeaders, httpStatus);
   }
 }
